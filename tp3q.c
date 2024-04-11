11e97d7ff8a54ce9a838bd9592e031723d15cbee
valerixb <valerio.bassetti@gmail.com>
2024-04-11 09:54:38 +0200

    convert tp ID to big endian in header (RX)

--- tp3q.c
+++ tp3q.c
@@ -9,7 +9,7 @@
  * See Xilinx PG080 document for IP details:
  * https://docs.xilinx.com/r/en-US/pg080-axi-fifo-mm-s/AXI4-Stream-FIFO-LogiCORE-IP-Product-Guide
  * 
- * latest rev by valerix, nov 24 2023
+ * latest rev by valerix, dec 15 2023
  * 
  */
 
@@ -35,6 +35,7 @@
 #include <linux/types.h>
 #include <linux/uaccess.h>
 #include <linux/jiffies.h>
+#include <asm/byteorder.h>
 
 #include <linux/of_address.h>
 #include <linux/of_device.h>
@@ -1267,7 +1268,7 @@ static ssize_t axis_fifo_read(struct file *f, char __user *buf,
 
     // first word is the number of the timepix that transmitted the data = TDEST of the bus,
     // which is stored by the MM2S FIFO into RDR = receive destination register
-    tmp_buf[0]=ioread32(fifo->base_addr + XLLF_RDR_OFFSET) & XLLF_RDR_RDEST_MASK;
+    tmp_buf[0]=cpu_to_be32(ioread32(fifo->base_addr + XLLF_RDR_OFFSET) & XLLF_RDR_RDEST_MASK);
     if (copy_to_user(buf, tmp_buf, sizeof(u32))) {
             reset_ip_core(fifo);
             return -EFAULT;
